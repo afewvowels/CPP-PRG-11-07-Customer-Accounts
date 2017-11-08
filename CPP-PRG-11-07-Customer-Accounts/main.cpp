cfa0ccc4476e723138324648880f8b8a427f352d
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-08 08:21:16 -0500

    Prompt written.

--- CPP-PRG-11-07-Customer-Accounts/main.cpp
+++ CPP-PRG-11-07-Customer-Accounts/main.cpp
@@ -5,11 +5,28 @@
 //  Created by Keith Smith on 11/8/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that uses a structure to store the following data about a customer
+//  account:
+//
+//      Name
+//      Address
+//      City, State, and ZIP
+//      Telephone Number
+//      Account Balance
+//      Date of Last Payment
+//
+//  The program should use an array of at least 10 structures. It should let the user enter
+//  data into the array, change the contents of any element, and display all the data stored
+//  in the array. The program should have a menu-driven user interface.
+//
+//  Input Validation: When the data for a new account is entered, be sure the user enters
+//  data for all the fields No negative account balances should be entered.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
     return 0;
 }
